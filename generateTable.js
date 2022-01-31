77d75c320b43b4862c34fe228b0cc021d8a7410f
Duy Pham Le <phamleduy04@gmail.com>
2022-01-31 15:13:51 -0600

    push

---
+++ generateTable.js
@@ -0,0 +1,43 @@
+const _ = require('lodash');
+module.exports = () => {
+    const board = [];
+    // board 9x9
+    // per row have 4 blank cells
+    for (let i = 1; i < 10; i++) {
+        let min, max;
+        if (i == 1) {
+            min = 1;
+            max = 10;
+        }
+        else {
+            min = (i - 1) * 10 + 1; 
+            max = i * 10;
+        }
+        board.push(_.shuffle(createArray(min, max)));
+    }
+    
+    return generateTable(transpose(board));
+
+};
+
+function createArray(min, max) {
+    const array = [];
+    for (let i = min; i < max; i++) array.push(i);
+    return array;
+}
+
+function transpose (matrix) {
+    let [row] = matrix
+    return row.map((value, column) => matrix.map(row => row[column]))
+}
+
+function generateTable(matrix) {
+    // remove random 4 cells per row
+    for (let i = 0; i < matrix.length; i++) {
+        const rowShuffled = _.shuffle(matrix[i]);
+        const toRemove = [];
+        for (let j = 0; j < 4; j++) toRemove.push(rowShuffled.shift())
+        for (let z = 0; z < matrix[i].length; z++) if (toRemove.includes(matrix[i][z])) matrix[i][z] = null;
+    }
+    return matrix;
+}
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,36 @@
 const router = require('express').Router();
+const fs = require('fs');
+const JSDOM = require('jsdom').JSDOM;
+const generateTable = require('../generateTable');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.status(200).send('Home page!');
+  const jsdom = new JSDOM(fs.readFileSync('./public/template.html', 'utf-8'));
+  const document = jsdom.window.document;
+  for (let i = 0; i < 9; i++) createCointainer(document);
+  const matrix = generateTable();
+  for (let i = 0; i < matrix.length; i++) {
+    matrix[i].forEach(el => appendNewThing(document, el, i));
+  }
+  fs.writeFileSync('test.html', document.documentElement.outerHTML);
+  res.status(200).send(document.documentElement.outerHTML);
 });
+
+function appendNewThing(document, el, i) {
+  let container = document.createElement("div");
+  container.className = "child-holder";
+  let newP = document.createElement("p");
+  newP.className = "child";
+  let newContent = document.createTextNode(!el ? '' : el);
+  newP.appendChild(newContent);
+  container.appendChild(newP);
+  document.getElementsByClassName("flex-container")[i].appendChild(container);
+}
+
+function createCointainer(document) {
+  const container = document.createElement("div");
+  container.className = 'flex-container';
+  document.body.appendChild(container);
+}
 
 module.exports = router;
