34db8e2b4439e26a71057ac00d55c0cc6a35bd48
Duy Pham Le <phamleduy04@gmail.com>
2022-02-02 13:19:25 -0600

    remove jquery :+1:

--- public/script/randomColor.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(() => {
-    $(".blank").css("background-color", "hsla(" + Math.floor(Math.random() * (360)) + ", 75%, 58%, 1)");
-    window.localStorage.clear();
-});
--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,20 @@ router.get('/', (req, res, next) => {
   const jsdom = new JSDOM(fs.readFileSync('./public/template.html', 'utf-8'));
   const document = jsdom.window.document;
   const matrix = generateTable();
+  const randomColor = `hsla(${Math.floor(Math.random() * (360))}, 75%, 58%, 1)`;
   for (let i = 0; i < matrix.length; i++) {
     createCointainer(document);
-    matrix[i].forEach((el, index) => appendNewCell(document, el, i, index));
+    matrix[i].forEach((el, index) => appendNewCell(document, el, i, index, randomColor));
   }
   res.status(200).send(document.documentElement.outerHTML);
 });
 
-function appendNewCell(document, el, i, index) {
+function appendNewCell(document, el, i, index, randomColor) {
   let container = document.createElement("a");
-  if (!el) container.className =  "child-holder blank";
+  if (!el) {
+    container.className =  "child-holder blank";
+    container.setAttribute('style', `background-color: ${randomColor}`);
+  }
   else container.className = "child-holder";
   container.href = `javascript:changeClass(document.getElementsByClassName("flex-container")[${i}].getElementsByClassName("child-holder")[${index}], ${i}, ${index})`;
   let newP = document.createElement("p");
