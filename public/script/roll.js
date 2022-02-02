6db8609e8e737eec215aea188d6c0c7145a3eab0
Duy Pham Le <phamleduy04@gmail.com>
2022-02-02 11:23:24 -0600

    add lodash and 3 button

---
+++ public/script/roll.js
@@ -0,0 +1,25 @@
+let numbers = [];
+for (let i = 1; i < 100; i++) numbers.push(i);
+numbers = _.shuffle(numbers);
+const pickedList = [];
+function roll() {
+    const pick = numbers.shift();
+    alert(pick);
+    pickedList.push(pick);
+}
+
+function list() {
+    if (pickedList.length === 0) return alert("Danh sách trống!");
+    alert(pickedList.join(" "));
+}
+
+function verify() {
+    const numbers = prompt("Nhập các số cần kiểm tra (cách nhau bởi dấu cách)\nVD: 30 69 44");
+    if (numbers === null) return;
+    const list = numbers.split(" ");
+    if (list.length == 0) return;
+    const result = _.difference(list.map(el => parseInt(el)), pickedList).filter(el => !isNaN(el));
+    if (result.length !== 0) return alert(`Số ${result.join(" ")} không nằm trong danh sách!`);
+    return alert('Tất cả các số bạn đưa đều trong danh sách!');
+    
+}
