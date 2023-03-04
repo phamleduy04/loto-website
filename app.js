19f2a5895315adbc9adb900cdffb753eaf84df20
Duy Pham Le <32657584+phamleduy04@users.noreply.github.com>
2023-03-03 23:04:18 -0600

    remove pug

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ const indexRouter = require('./routes/index');
 require('dotenv').config();
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
